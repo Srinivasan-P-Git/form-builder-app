122ff20c2021ab4a006890f65b125b4738b90005
SrinuThala <sriniv2s2n@gmail.com>
2022-09-03 17:24:45 +0530

    Changes to Index.js and Package.json file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+
+ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
+  root
 );
